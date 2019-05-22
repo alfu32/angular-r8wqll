0045918dc1e2fb4c25ace696d44262daa4fb704a
alfu32 <ioan.alferaru@gmail.com>
2019-05-22 20:07:48 +0200

    3 files modified

--- src/app/input2/input2.component.ts
+++ src/app/input2/input2.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,ViewChild,ElementRef } from '@angular/core';
+import { Component, OnInit, Input ,ViewChild,ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-input2',
@@ -6,24 +6,38 @@ import { Component, OnInit,ViewChild,ElementRef } from '@angular/core';
   styleUrls: ['./input2.component.scss']
 })
 export class Input2Component implements OnInit {
+  @Input("mask") mask:string;
   @ViewChild('outputview') outputview: ElementRef;
   @ViewChild('inputview') inputview: ElementRef;
+  @ViewChild('cursorview') cursorView: ElementRef;
   instring
   outstring
   inStrpos
   outStrpos
   constructor() { }
-  fn(str,p){
-    return str;
+  tf(str){
+    return str.match(/.{1,3}/g).join("_");
   }
-  pos(str,p){
-    return p;
+  fn(str,p){
+    let strIn=this.tf(str);
+    let pOut=Number.parseInt(p*4/3 + "");
+    return `${strIn.substr(0,pOut)}<cursor></cursor>${strIn.substr(pOut)}`;
+    return strIn.split("").reverse().join("");
+  }
+  pos(strIn,pIn,strOut){
+    return strIn.length-pIn;
   }
   _common(event){
-    this.instring=event.target.innerHTML;
+    //console.log(event);
+    this.instring=event.target.value;
     this.inStrpos=event.target.selectionStart;
     this.outstring=this.fn(this.instring,this.inStrpos);
-    this.outStrpos=this.pos(this.outstring,this.inStrpos);
+    this.outputview.nativeElement.innerHTML=this.outstring;
+    const cursor = this.outputview.nativeElement.querySelectorAll("cursor");
+
+    this.outStrpos=cursor[0].offsetLeft;//this.pos(this.instring,this.inStrpos,this.outstring);
+    this.cursorView.nativeElement.style.left = `${cursor[0].offsetLeft}px`;
+    this.cursorView.nativeElement.style.top = `${cursor[0].offsetTop}px`;
   }
   inputKeyUp(event){
     this._common(event)
